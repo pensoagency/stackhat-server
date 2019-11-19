4c4a29e25f426c02cb83e003e023b9c449157ff2
Paul Stephenson <paul@impel.co>
2019-11-19 12:02:40 +1100

    Initial commit REST api.

---
+++ StackhatServer/src/main/java/penso/stackhat/DatabaseRequest.java
@@ -0,0 +1,8 @@
+package penso.stackhat;
+
+public class DatabaseRequest {
+    String[] urls = new String[0];
+
+    public String[] getUrls() { return urls; }
+    public void setUrls(String[] urls) { this.urls=urls; }
+}
---
+++ StackhatServer/src/main/java/penso/stackhat/Databases.java
@@ -0,0 +1,39 @@
+package penso.stackhat;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at "databases" path)
+ */
+@Path("databases")
+public class Databases {
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Got it!";
+    }
+    
+    /**
+     * Method handling HTTP POST requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String postIt(DatabaseRequest request) {
+        return "Got it!";
+    }
+}
