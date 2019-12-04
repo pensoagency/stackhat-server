5bdd1b246ec67866a37be9b32c3143fa7a22b4ad
Paul Stephenson <paul@impel.co>
2019-12-05 03:10:26 +1100

    Audit get.

--- StackhatServer/src/main/java/penso/stackhat/server/rest/Audits.java
+++ StackhatServer/src/main/java/penso/stackhat/server/rest/Audits.java
@@ -1,18 +1,25 @@
 package penso.stackhat.server.rest;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
+import javax.json.Json;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
@@ -21,6 +28,7 @@ import javax.ws.rs.core.StreamingOutput;
 import javax.ws.rs.core.Response.ResponseBuilder;
 
 import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import penso.stackhat.builtwith.*;
 import penso.stackhat.server.filter.JWTTokenNeeded;
@@ -29,22 +37,63 @@ import penso.stackhat.server.model.NewAuditRequest;
 import penso.stackhat.server.util.ParamRunnable;
 
 /**
- * Root resource (exposed at "databases" path)
+ * Root resource (exposed at "audits" path)
  */
 @Path("/audits")
 public class Audits {
-    // /**
-    // * Method handling HTTP GET requests. The returned object will be sent to the
-    // * client as "text/plain" media type.
-    // *
-    // * @return String that will be returned as a text/plain response.
-    // */
-    // @GET
-    // @Produces(MediaType.TEXT_PLAIN)
-    // @JWTTokenNeeded
-    // public String getIt() {
-    // return "Hello World!";
-    // }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @JWTTokenNeeded
+    public Response getIt(String id) {
+
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(Program.pathAuditsBase + id + ".json"));
+
+            // return
+            return Response.ok(lines.toString()).build();
+
+        } catch (FileNotFoundException e) {
+            return Response.status(404).build();
+        } catch (Exception e) {
+            return Response.status(500).build();
+        }
+
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @JWTTokenNeeded
+    public Response getAll() {
+
+        try
+        {
+            ArrayList<Object> result = new ArrayList<Object>();
+
+            File dir = new File(Program.pathAuditsBase);
+            File [] files = dir.listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    return name.endsWith(".json");
+                }
+            });
+
+            JSONParser parser = new JSONParser();
+            for (File jsonFile : files) {
+                result.add(parser.parse(new FileReader(jsonFile.getAbsolutePath())));
+            }
+
+            // return 
+            return Response.ok(result)
+                .build();
+ 
+        } catch (FileNotFoundException e) {
+            return Response.status(404).build();
+        } catch (Exception e) {
+            return Response.status(500).build();
+        }
+
+    }    
 
     /**
      * Method handling HTTP POST requests. The returned object will be sent to the
@@ -100,27 +149,6 @@ public class Audits {
             });
 
             return Response.ok(result).build();
-
-            // // return file
-            // StreamingOutput fileStream = new StreamingOutput()) {
-            // @Override
-            // public void write(java.io.OutputStream output) throws IOException,
-            // WebApplicationException {
-            // try {
-            // String path = pathDestination;
-
-            // java.nio.file.Path filePath = Paths.get(path);
-            // byte[] data = Files.readAllBytes(filePath);
-            // output.write(data);
-            // output.flush();
-            // } catch (Exception e) {
-            // throw new WebApplicationException("File Not Found !!");
-            // }
-            // }
-            // };
-
-            // return Response.ok(fileStream, MediaType.APPLICATION_OCTET_STREAM)
-            // .header("content-disposition", "attachment; filename = file.xlsx").build();
 
         } catch (Exception ex) {
             return Response.serverError().build();
